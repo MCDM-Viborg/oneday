e998d27803b06be6d112ac3da50151b871a6cc21
Anne <anne@mediacollege.dk>
2024-05-14 20:11:14 +0200

    Optimize api-call
    Closes #6

--- src/components/gallerySection/GallerySection.jsx
+++ src/components/gallerySection/GallerySection.jsx
@@ -1,22 +1,27 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import GalleryImage from "./GalleryImage/GalleryImage";
-import "./GallerySection.css"
+import "./GallerySection.css";
 
-const GallerySection = ({data, time, description}) => {
-    
+const GallerySection = ({ data, description, time }) => {
+  return (
+    <div className='gallerySection-container'>
+      {/* Render kun headeren hvis der er data */}
+      {data.length > 0 && (
+        <header className='gallerySection-container-inner'>
+          <h2 className='gallerySection-time'>{time}</h2>
+          <h1 className='gallerySection-container-heading'>{description}</h1>
+        </header>
+      )}
 
-
-    return <div className="gallerySection-container">
-        <div className="gallerySection-container-inner">
-            <h1 className="gallerySection-container-heading">{time}</h1>
-            <h1 className="gallerySection-container-heading-second">{description}</h1>
-        </div>
-        <div className="gallery-section-container-images">
-           
-            {data.map((obj) => <Link to={`single/${obj._id}`} key={obj._id}><GalleryImage data={obj}></GalleryImage></Link>)}
-        </div>
+      <div className='gallery-section-container-images'>
+        {data.map((obj) => (
+          <Link to={`single/${obj._id}`} key={obj._id}>
+            <GalleryImage data={obj} />
+          </Link>
+        ))}
+      </div>
     </div>
-
+  );
 };
 
 export default GallerySection;
