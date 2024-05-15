85d883a0f8b4b919f7bf15eab3184b9ff0d810d7
Anne <anne@mediacollege.dk>
2024-05-15 10:03:23 +0200

    Hide the about-page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
       <Route path='/single/:id' element={<Single></Single>}></Route>
-      <Route path='/om' element={<About></About>}></Route>
+      {/* <Route path='/om' element={<About></About>}></Route> */}
       <Route path='/credits' element={<Credits></Credits>}></Route>
       <Route path='/historie' element={<History></History>}></Route>
     </Routes>
--- src/components/aboutHeader/AboutHeader.jsx
+++ src/components/aboutHeader/AboutHeader.jsx
@@ -1,12 +1,23 @@
-import "./AboutHeader.css"
+import "./AboutHeader.css";
 
 const AboutHeader = () => {
-
-    return <div className="aboutContainer-header">
-        <h1 className="aboutContainer-header-heading">ONE DAY VIBORG</h1>
-        <p className="aboutContainer-header-text">Vi tror ikke det før er sket, at 60 fotografer har gennemfotograferet Viborg over et døgn. Men nu er det sket og nu har du muligheden for at se op mod 800 billeder fordelt over 90 fotoserier, som er taget i 8800 Viborg d. 11. maj 2022. Fotograferne har været ude i folks hjem, ude i insitutionerne, på virksomhederne, i gaderne og i klubberne og rigtig mange andre steder. De har med både almindelige og ualmindelige historier og billeder skildret en helt almindelig dag i Viborg. Projektet er planlagt og gennemført af Medieskolerne i Viborg. Skolens fotografelever har taget billederne og skolens webudviklerelever har bygget hjemmesiden onedayviborg.dk</p>
+  return (
+    <div className='aboutContainer-header'>
+      <h1 className='aboutContainer-header-heading'>ONE DAY VIBORG</h1>
+      <p className='aboutContainer-header-text'>
+        Oplev Viborg som aldrig før! Den 16. maj 2024 blev byen indfanget i en
+        enestående fotografisk maraton, hvor omkring 40 talentfulde fotografer
+        fra Medieskolerne i Viborg tog ud på en uforglemmelig rejse gennem byens
+        hjerte og sjæl. Ca. 800 billeder giver et sjældent indblik i
+        hverdagslivet i 8800 Viborg. Vores fotografelever har fanget alt fra
+        intime øjeblikke i private hjem til pulserende liv i gader,
+        institutioner, virksomheder og klubber. Webudviklerelever fra skolen har
+        desuden designet og opbygget hjemmesiden onedayviborg.dk, hvor alle
+        disse historier og billeder er samlet til din oplevelse. Dyk ned i dette
+        fascinerende projekt og se Viborg, som du aldrig har set det før!
+      </p>
     </div>
-
+  );
 };
 
 export default AboutHeader;
