2984122d542a60f346d70908f4bfd5e752b80eff
Mathias Lund Eriksen <141811220+Mathiaslunderiksen@users.noreply.github.com>
2024-05-16 12:44:51 +0200

    sound control header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,19 @@
+import { useRef, useState } from "react";
 import "./Header.css";
+import {GoUnmute, GoMute} from "react-icons/go";
 
 const Header = () => {
+  const videoRef = useRef(null);
+  const [isMuted, setIsMuted] = useState(true);
+
+  const toggleMute = () => {
+    if(videoRef.current){
+      videoRef.current.muted = !videoRef.current.muted;
+      setIsMuted(videoRef.current.muted)
+    }
+  }
+
+
   return (
     <div className='header-container'>
       
@@ -8,19 +21,25 @@ const Header = () => {
         <video
           src='https://player.vimeo.com/progressive_redirect/playback/946905222/rendition/1080p/file.mp4?loc=external&log_user=0&signature=bc015b88aa8ac439e2f7eb160c1c1751ef36bc3d170833a169514193ae792307'
           className='bg-video'
-          controls
+          ref={videoRef}
           autoPlay
           muted
           loop>
         </video>
 
-                    
-        <div className='header-paragraph'>
-          <p>
-            En hel masse facetter fra livet i Viborg dokumenteret d. 16. maj
-            2024 af fotograf- og webudviklerstuderende ved Media College
-            Denmark / Medieskolerne
-          </p>
+  
+        <div className="header-grid-inner">
+                      
+          <div className='header-paragraph'>
+            <p>
+              En hel masse facetter fra livet i Viborg dokumenteret d. 16. maj
+              2024 af fotograf- og webudviklerstuderende ved Media College
+              Denmark / Medieskolerne
+            </p>
+          </div>
+          <button className="muteBtn" onClick={toggleMute}>
+                  {isMuted ? <GoMute /> : <GoUnmute />}
+              </button>
         </div>
           
 
