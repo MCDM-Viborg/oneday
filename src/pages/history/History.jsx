a225b21798846fbcda5259becd09ad01b575d70d
Anne <anne@mediacollege.dk>
2024-05-16 13:33:24 +0200

    panic-fix

---
+++ src/pages/history/History.jsx
@@ -0,0 +1,27 @@
+import "./History.css";
+
+export function History() {
+  return (
+    <div className='historyContainer'>
+      <figure>
+        <img src='./images/stiftsTidende.jpg' alt='Viborg Stiftstidende' />
+        <figcaption>
+          <h2>Vi kigger 100 år tilbage</h2>
+          For 100 år siden eksisterede der to store aviser i Viborg, henholdsvis
+          Viborg Stifts Folkeblad og Viborg Stiftstidende. Sidstnævnte ophørte i
+          1962 efter næsten 200 år på gaden. Viborg Stifts Folkeblad har vi
+          endnu og den blev grundlagt i 1877. Men hvad bragte de to aviser d.
+          11. maj 1922? Vi har kigget i 'Mediestream' og fundet 7 ting frem,
+          herunder både nyhedsstof, læserbreve og nogle forskellige indlæg. Vi
+          har forkortet teksterne, men ikke ændret gamle ord. Til gengæld har vi
+          for læsevenlighedens skyld ændret aa til å og versaler til minuskler
+          på de navneord, som ikke er stednavne eller personnavne. Der var ikke
+          mange billeder i aviserne dengang. Derfor har vi indsat annoncer ud
+          for de fleste artikler - så fik vi også nogle af dem med og skabt lidt
+          variation og visualitet på siden. Redaktionen ONE DAY VIBORG,
+          Medieskolerne
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
