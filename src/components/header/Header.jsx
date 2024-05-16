660b1c7f796aa99db3a0bdb2180bfb534b18f1ab
Anne <anne@mediacollege.dk>
2024-05-16 16:47:26 +0200

    video-fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,35 +1,30 @@
 import { useRef, useState } from "react";
 import "./Header.css";
-import {GoUnmute, GoMute} from "react-icons/go";
+import { GoUnmute, GoMute } from "react-icons/go";
 
 const Header = () => {
   const videoRef = useRef(null);
   const [isMuted, setIsMuted] = useState(true);
 
   const toggleMute = () => {
-    if(videoRef.current){
+    if (videoRef.current) {
       videoRef.current.muted = !videoRef.current.muted;
-      setIsMuted(videoRef.current.muted)
+      setIsMuted(videoRef.current.muted);
     }
-  }
-
+  };
 
   return (
     <div className='header-container'>
-      
-      <section className="header-grid">
+      <section className='header-grid'>
         <video
           src='https://player.vimeo.com/progressive_redirect/playback/946905222/rendition/1080p/file.mp4?loc=external&log_user=0&signature=bc015b88aa8ac439e2f7eb160c1c1751ef36bc3d170833a169514193ae792307'
           className='bg-video'
           ref={videoRef}
           autoPlay
           muted
-          loop>
-        </video>
+          loop></video>
 
-  
-        <div className="header-grid-inner">
-                      
+        <div className='header-grid-inner'>
           <div className='header-paragraph'>
             <p>
               En hel masse facetter fra livet i Viborg dokumenteret d. 16. maj
@@ -37,15 +32,11 @@ const Header = () => {
               Denmark / Medieskolerne
             </p>
           </div>
-          <button className="muteBtn" onClick={toggleMute}>
-                  {isMuted ? <GoMute /> : <GoUnmute />}
-              </button>
+          <button className='muteBtn' onClick={toggleMute}>
+            {isMuted ? <GoMute /> : <GoUnmute />}
+          </button>
         </div>
-          
-
-
       </section>
-      
     </div>
   );
 };
